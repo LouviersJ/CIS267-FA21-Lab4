0f165dc20da6bc3224316adf9eecd22064c2a4ad
LouviersJ <lou.joseph@students.fhu.edu>
2021-11-17 02:28:57 -0600

    A lot

--- app.js
+++ app.js
@@ -1,33 +1,133 @@
-const app= Vue.createApp(
-    {
-    data(){
-        return{
-        allPokemon: [{id:1, name:"bulbasaur", type:"grass"}, {id:1, name:"ivysaur", type:"grass"} ],
-        partyPokemon: [{id:1, name:"ivysaur", type:"grass"}],
+const getPokemon = async function (id) {
+    // get pokemon data from pokeapi
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    return data;
+    //createPokemonCard( data );
+  };
+  
+  const app = Vue.createApp({
+    data() {
+      return {
+        allPokemon: [],
+        partyPokemon: [],
         filteredPokemon: [],
-        maxPartySize: 6
-        };
-
+        maxPartySize: 6,
+        inputValue: "",
+        colors: {
+          fire: "#fd7d24",
+          grass: "#9bcc50",
+          electric: "#eed535",
+          water: "#4592c4",
+          ground: "#ab9842",
+          rock: "#a38c21",
+          fairy: "#fdb9e9",
+          poison: "#b97fc9",
+          bug: "#729f3f",
+          dragon: "#7038f8",
+          psychic: "#f366b9",
+          flying: "#3dc7ef",
+          fighting: "#d56723",
+          normal: "#a4acaf",
+          ice: "#51c4e7",
+          ghost: "#7b62a3",
+          dark: "#707070",
+          steel: "#9eb7b8",
+        },
+      };
     },
     methods: {
-        loadPokemon() {
-            //load all pokemon from API and save into all pokemon
-        },
-        filterPokemon() {
-            //set the filtered pokemon to matching pokemon based on search query
-        },
-        addPokemonToParty(pokemon) {
-
-        },
-        removePokemonFromParty(pokemon) {
-
-        },
-        pokemonTypeString(pokemon){
-            if(pokemon.types.length > 1) {
-                return '${pokemon.types.type.name}';
-            }
+      clearPokemon() {
+        this.filteredPokemon = [];
+      },
+      async loadPokemon() {
+        // load all pokemon from API and save into all pokemon
+        const pokemon_count = 150;
+        let pokemon = [];
+        for (let i = 1; i <= pokemon_count; i++) {
+          let p = await getPokemon(i);
+          if (p.name === "mr-mime") {
+            p.name = "Mr. Mime";
+          }
+          p.isFavorite = false;
+          pokemon.push(p);
         }
-
-    }
-}).mount("#app");
-html
+  
+        pokemon.forEach((p) => {
+          this.allPokemon.push(p);
+        });
+      },
+      filterPokemon(inputValue) {
+        // set filteredPokemon to matching pokemon based on search query
+        this.filteredPokemon = [];
+        this.allPokemon.filter((pokemon) => {
+          if (pokemon.name.toLowerCase().includes(inputValue.toLowerCase())) {
+            const pokemonCopy = { ...pokemon };
+            pokemonCopy.guid = this.getGUID();
+            this.filteredPokemon.push(pokemonCopy);
+          }
+          else if (pokemon.types.length > 1) {
+            if (
+              pokemon.types[1].type.name
+                .toLowerCase()
+                .includes(inputValue.toLowerCase())
+            ) {
+              const pokemonCopy = { ...pokemon };
+              pokemonCopy.guid = this.getGUID();
+              this.filteredPokemon.push(pokemonCopy);
+            }
+          }
+          else if (
+            pokemon.types[0].type.name
+              .toLowerCase()
+              .includes(inputValue.toLowerCase())
+          ) {
+            const pokemonCopy = { ...pokemon };
+            pokemonCopy.guid = this.getGUID();
+            this.filteredPokemon.push(pokemonCopy);
+          }
+          else if (pokemon.id.toString().includes(inputValue.toLowerCase())) {
+            const pokemonCopy = { ...pokemon };
+            pokemonCopy.guid = this.getGUID();
+            this.filteredPokemon.push(pokemonCopy);
+          }
+        });
+      },
+      addPokemonToParty(pokemon) {
+        if (this.partyPokemon.length < 6) {
+          const pokemonCopy = { ...pokemon };
+          pokemonCopy.guid = this.getGUID();
+          console.log(pokemonCopy.guid);
+          this.partyPokemon.push(pokemonCopy);
+        }
+      },
+      removePokemonFromParty(pokemon) {
+        this.partyPokemon = this.partyPokemon.filter(
+          (p) => p.guid != pokemon.guid
+        );
+      },
+      pokemonTypeString(pokemon) {
+        if (pokemon.types.length > 1) {
+          return `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`;
+        } else {
+          return `${pokemon.types[0].type.name}`;
+        }
+      },
+      getGUID() {
+        return Math.floor(Math.random() * 1000000);
+      },
+    },
+    computed: {
+      createBackgroundString() {
+        return `linear-gradient(90deg, ${colors[pokemon.types[0].type.name]} 50%, ${colors[pokemon.types.length > 1 ? 
+          pokemon.types[1].type.name : pokemon.types[0].type.name]})`;
+      }
+    },
+  
+    mounted() {
+      this.loadPokemon();
+    },
+  }).mount("#app");
